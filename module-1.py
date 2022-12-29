1ed7d4261cf3b28833f2866f2d36b499d9988c1d
Hessum <hessumg@gmail.com>
2022-12-29 20:04:53 +0330

    Change the coding tyoe to the class based one

--- module-1.py
+++ module-1.py
@@ -1,56 +1,63 @@
 import json
 
-path = ''
-#path = 'C:/Users/Hessum/Desktop/'
-path_in = path + 's22.txt'
-
-with open(path_in) as f:
-    lines = f.readlines()
-
-keywords = ['NETWORK', 'LAUNCH', 'BODY', 'DISPLAY', 'PLATFORM', 'MEMORY', 'MAIN CAMERA', 
+class GSM_Arena:
+    
+    def __init__(self, path='', fileName=''):
+        path_in = path + fileName
+        with open(path_in) as f:
+            self.lines = f.readlines()
+        self.keywords = ['NETWORK', 'LAUNCH', 'BODY', 'DISPLAY', 'PLATFORM', 'MEMORY', 'MAIN CAMERA', 
             'SELFIE CAMERA', 'SOUND', 'COMMS', 'FEATURES', 'BATTERY', 'MISC', 'TESTS']
-dict = {}
-for j in range(len(keywords)):                                  # Crawl the keywords
-
-    keyword = keywords[j]                                       # Set the keyword as a major KEY
-    try:
-        next_keyword = keywords[j+1]                            # Set the next keyword
-    except:
-        next_keyword = 'Disclaimer'
-
-    for i in range(len(lines)):                                 # Read each line separately
-        line = lines[i].strip()
         
-        if line.split('\t')[0] == keyword:                      # Find the first minor key
-            key = line.split('\t')[1]
-            dict[keyword] = {key: ''}                          # Make a dictionary for the major KEY
+    def txt2dict(self):
+        dict = {}
+        for j in range(len(self.keywords)):                                  # Crawl the keywords
 
-            while lines[i].strip().split('\t')[0] != next_keyword:
-                list = lines[i].strip().split('\t')
+            keyword = self.keywords[j]                                       # Set the keyword as a major KEY
+            try:
+                next_keyword = self.keywords[j+1]                            # Set the next keyword
+            except:
+                next_keyword = 'Disclaimer'
+
+            for i in range(len(self.lines)):                                 # Read each line separately
+                line = self.lines[i].strip()
                 
-                if list[-1] == key: 
-                    dict[keyword][key] = [lines[i+1].strip().split('\t')[0]]
-                    i += 1
-                
-                else:
-                    #print(list)
+                if line.split('\t')[0] == keyword:                      # Find the first minor key
+                    key = line.split('\t')[1]
+                    dict[keyword] = {key: ''}                          # Make a dictionary for the major KEY
 
-                    if len(list) == 3:
-                        key = list[1]
-                        dict[keyword][key] = [list[2]]
+                    while self.lines[i].strip().split('\t')[0] != next_keyword:
+                        list = self.lines[i].strip().split('\t')
+                        
+                        if list[-1] == key: 
+                            dict[keyword][key] = [self.lines[i+1].strip().split('\t')[0]]
+                            i += 1
+                        
+                        else:
+                            #print(list)
 
-                    if len(list) == 2:
-                        key = list[0]
-                        dict[keyword][key] = [list[1]]
+                            if len(list) == 3:
+                                key = list[1]
+                                dict[keyword][key] = [list[2]]
 
-                    if len(list) == 1 and list[0] not in dict[keyword][key]:
-                        dict[keyword][key].append(list[0])
+                            if len(list) == 2:
+                                key = list[0]
+                                dict[keyword][key] = [list[1]]
 
-                    i += 1
-                    if next_keyword == 'Disclaimer': break
+                            if len(list) == 1 and list[0] not in dict[keyword][key]:
+                                dict[keyword][key].append(list[0])
+
+                            i += 1
+                            if next_keyword == 'Disclaimer': break
+
+        return dict
+
+    def dict2json(self, dict, name):
+        fileName = name + '.json'
+        with open(fileName, 'w') as f:
+            json.dump(dict, f)
 
 
-print(dict)
-
-with open('data.json', 'w') as f:
-    json.dump(dict, f)
+object = GSM_Arena(fileName='s22.txt')
+dict = object.txt2dict()
+object.dict2json(dict, 's22')
